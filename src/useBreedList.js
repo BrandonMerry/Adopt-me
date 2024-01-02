578f40e84dad09c701e1229b65b5c8927f42612c
Brandon Merry <Brandon.l.merry@gmail.com>
2024-01-02 10:17:22 +0700

    react Query

--- src/useBreedList.js
+++ src/useBreedList.js
@@ -1,50 +1,8 @@
-import { useState, useEffect } from "react";
-
-// THIS IS A CUSTOM HOOK (OTHER HOOKS PACKAGED TOGETHER)
-// HOOKS ARE 'useState" &
-
-// local cache because the breed probably hasnt updated in 13s
-// basically stores values in browser
-const localCache = {};
-
-// this is  function to request a new breedlist when an aniaml is selected
+import { useQuery } from "@tanstack/react-query";
+import fetchBreedList from "./fetchBreedList";
+// custom hook was deleted and replaced iwth fetch breedlist
 
 export default function useBreedList(animal) {
-  // breedlist is not pupulated to start
-  const [breedList, setBreedList] = useState([]);
-  // status is not unloaded to start
-  const [status, setStatus] = useState("unloaded");
-
-  useEffect(() => {
-    // if no(!) animal, breedlist is empty[]
-    if (!animal) {
-      setBreedList([]);
-    }
-    // or if there check the local cache for the animal and populate
-    else if (localCache[animal]) {
-      setBreedList(localCache[animal]);
-    }
-    // or return the breed list
-    else {
-      requestBreedList();
-    }
-
-    // asyn funtion tells the status and if the list is loading
-
-    async function requestBreedList() {
-      setBreedList([]);
-      setStatus("loading");
-      // await and feth data form this api
-      const res = await fetch(
-        `http://pets-v2.dev-apis.com/breeds?animal=${animal}`
-      );
-      const json = await res.json();
-      //   load local cache or load nothing
-      localCache[animal] = json.breeds || [];
-      setBreedList(localCache[animal]);
-      setStatus("loaded");
-    }
-  }, [animal]);
-
-  return [breedList, status];
+  const results = useQuery(["breeds", animal], fetchBreedList);
+  return [results?.data?.breeds ?? [], results.status];
 }
